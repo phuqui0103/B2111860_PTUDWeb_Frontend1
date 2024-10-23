013cce6689e616e48ade22136c5e0faffbaf293c
Vo Phu Qui <145702431+phuqui0103@users.noreply.github.com>
2024-10-24 03:06:35 +0700

    Hien thi danh sach cac contact lay ve tu sever

---
+++ src/views/ContactBook.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="page row">
+    <div class="col-md-10">
+      <InputSearch v-model="searchText" />
+    </div>
+    <div class="mt-3 col-md-6">
+      <h4>
+        Danh bạ
+        <i class="fas fa-address-book"></i>
+      </h4>
+      <ContactList
+        v-if="filteredContactsCount > 0"
+        :contacts="filteredContacts"
+        v-model:activeIndex="activeIndex"
+      />
+      <p v-else>Không có liên hệ nào.</p>
+      <div class="mt-3 row justify-content-around align-items-center">
+        <button class="btn btn-sm btn-primary" @click="refreshList">
+          <i class="fas fa-redo"></i> Làm mới
+        </button>
+        <button class="btn btn-sm btn-success" @click="goToAddContact">
+          <i class="fas fa-plus"></i> Thêm mới
+        </button>
+        <button class="btn btn-sm btn-danger" @click="removeAllContacts">
+          <i class="fas fa-trash"></i> Xóa tất cả
+        </button>
+      </div>
+    </div>
+    <div class="mt-3 col-md-6">
+      <div v-if="activeContact">
+        <h4>
+          Chi tiết Liên hệ
+          <i class="fas fa-address-card"></i>
+        </h4>
+        <ContactCard :contact="activeContact" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContactCard from "@/components/ContactCard.vue";
+import InputSearch from "@/components/InputSearch.vue";
+import ContactList from "@/components/ContactList.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  components: {
+    ContactCard,
+    InputSearch,
+    ContactList,
+  },
+  data() {
+    return {
+      contacts: [],
+      activeIndex: -1,
+      searchText: "",
+    };
+  },
+  watch: {
+    searchText() {
+      this.activeIndex = -1; // Bỏ chọn khi tìm kiếm mới
+    },
+  },
+  computed: {
+    // Trả về các contact chứa thông tin tìm kiếm
+    filteredContacts() {
+      if (!this.searchText) return this.contacts;
+      const searchLower = this.searchText.toLowerCase();
+      return this.contacts.filter((contact) => {
+        const contactString =
+          `${contact.name} ${contact.email} ${contact.address} ${contact.phone}`.toLowerCase();
+        return contactString.includes(searchLower);
+      });
+    },
+    activeContact() {
+      if (this.activeIndex < 0) return null;
+      return this.filteredContacts[this.activeIndex];
+    },
+    filteredContactsCount() {
+      return this.filteredContacts.length;
+    },
+  },
+  methods: {
+    async retrieveContacts() {
+      try {
+        this.contacts = await ContactService.getAll();
+      } catch (error) {
+        console.error("Error retrieving contacts:", error);
+      }
+    },
+    refreshList() {
+      this.retrieveContacts();
+      this.activeIndex = -1; // Reset danh sách sau khi làm mới
+    },
+    async removeAllContacts() {
+      if (confirm("Bạn muốn xóa tất cả Liên hệ?")) {
+        try {
+          await ContactService.deleteAll();
+          this.refreshList();
+        } catch (error) {
+          console.error("Error deleting all contacts:", error);
+        }
+      }
+    },
+    goToAddContact() {
+      this.$router.push({ name: "contact.add" });
+    },
+  },
+  mounted() {
+    this.refreshList(); // Lấy danh bạ khi component được khởi tạo
+  },
+};
+</script>
+
+<style scoped>
+.page {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
